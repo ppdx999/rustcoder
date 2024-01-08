4af8daad954d5ae6f8f1e0694eb1bf7cf980bb60
ppdx999 <ppdx999@gmail.com>
2024-01-08 15:13:31 +0900

    solve abc335_c

--- abc335/src/bin/c.rs
+++ abc335/src/bin/c.rs
@@ -1,3 +1,34 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! { n: usize, q: usize }
+    let mut v = vec![];
+
+    for i in (1..=n).rev() {
+        v.push((i as isize, 0 as isize));
+    }
+
+    for _ in 0..q {
+        input! { cmd: usize }
+        match cmd {
+            1 => {
+                input! { c: char }
+                let (px, py) = *v.last().unwrap();
+
+                match c {
+                    'R' => v.push((px + 1, py)),
+                    'L' => v.push((px - 1, py)),
+                    'U' => v.push((px, py + 1)),
+                    'D' => v.push((px, py - 1)),
+                    _ => unreachable!(),
+                }
+            }
+            2 => {
+                input! { p: usize }
+                let (x, y) = v[v.len() - p];
+                println!("{} {}", x, y);
+            }
+            _ => unreachable!(),
+        }
+    }
 }
